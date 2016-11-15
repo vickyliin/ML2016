99a39e25810e55306d990420fa92da29c7eb6299
Your Name <you@example.com>
2016-11-15 23:15:07 +0800

    Self-training  On branch self-training  Changes to be committed: 	new file:   test 	new file:   test.py 	modified:   train.py
     Changes not staged for commit:
    	modified:   test

---
+++ hw3/test.py
@@ -0,0 +1,56 @@
+from util import *
+import cPickle
+import numpy as np
+import sys
+import keras.backend
+from keras.models import Sequential
+from keras.layers import *
+from keras.optimizers import SGD, Adam
+from keras.callbacks import EarlyStopping
+from keras.models import load_model
+from train import myModel
+
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+keras.backend.tensorflow_backend.set_session(session)
+nb_classes=10
+
+if __name__ == '__main__':
+    path = 'data'
+    csvOut = 'prediction.csv'
+    modelIn = 'trained_model'
+
+    for i,arg in enumerate(sys.argv):
+        if arg.startswith('-dir'):
+            path = sys.argv[i+1]
+            if path[-1] == '/':
+                path = path[:-1]
+        if arg.startswith('-save'):
+            csvOut = sys.argv[i+1]
+        if arg.startswith('-load'):
+            modelIn = sys.argv[i+1]
+
+    # load/normalize/reordering data
+    print 'Loading data...'
+    Xl, Xu, Xt = load_data(path, data=['t'])
+    print 'Data Loaded!'
+
+    # load model
+    print 'Loading model...'
+    model = load_model(modelIn)
+    model.summary()
+    print 'Model Loaded!'
+
+    # predict/write
+    print 'Predicting the classes...'
+    Ct = model.predict_classes(Xt)
+    print 'Done!'
+    raw_input( 'Save as %s?' % csvOut )
+    with open(csvOut, 'w') as f:
+        f.write('ID,class\n')
+        for i, c in enumerate(Ct):
+            f.write('%d,%d\n' % (i,c))
+    print '%s saved!' % csvOut
+
--- hw3/train.py
+++ hw3/train.py
@@ -6,15 +6,17 @@ import keras.backend
 from keras.models import Sequential
 from keras.layers import *
 from keras.optimizers import SGD, Adam
+from keras.callbacks import EarlyStopping
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 keras.backend.tensorflow_backend.set_session(session)
+nb_classes=10
 
 class YModel(Sequential):
     def __init__(self, img_channels=3, img_rows=32,
-                 img_cols=32, nb_classes=10):
+                 img_cols=32, nb_classes=nb_classes):
         super(YModel, self).__init__()
         self.add(Convolution2D(64, 5, 5, dim_ordering='th',
                                 input_shape=(img_channels, img_rows, img_cols)))
@@ -48,7 +50,7 @@ class YModel(Sequential):
 class myModel(Sequential):
     def __init__(self, dim_ordering='th',
                  input_shape=(3,32,32),
-                 nb_classes=10, 
+                 nb_classes=nb_classes, 
                 ):
         super(myModel, self).__init__()
         self.add(Convolution2D(15, 5,5, dim_ordering=dim_ordering, input_shape=input_shape))
@@ -83,16 +85,20 @@ class myModel(Sequential):
         self.add(Activation('relu'))
         self.add(Activation('softmax'))
 
-        adam = Adam(lr=1e-3)
+        adam = Adam(lr=2e-3)
 
         self.compile(loss='categorical_crossentropy', optimizer=adam, init='lecun_uniform', metrics=['accuracy'])
         self.summary()
+    def train(self, X, Y, validation_split=0.2):
+        early_stopping = EarlyStopping(monitor='val_loss', patience=30)
+        return model.fit(X, Y, batch_size=300, nb_epoch=1000,
+                         validation_split=validation_split, 
+                         callbacks=[early_stopping], )
 
 if __name__ == '__main__':
     path = 'data'
     output_model = 'trained_model'
     input_model = output_model
-    test = False
 
     for i,arg in enumerate(sys.argv):
         if arg.startswith('-dir'):
@@ -101,19 +107,29 @@ if __name__ == '__main__':
                 path = path[:-1]
         if arg.startswith('-save'):
             output_model = sys.argv[i+1]
-        if arg.startswith('-test'):
-            test = True
         if arg.startswith('-load'):
             input_model = sys.argv[i+1]
 
     # load/normalize/reordering data
-    Xl, Xu, Xt = load_data(path, data=['l'])
+    Xl, Xu, Xt = load_data(path, data=['l','u'])
     Xl = np.swapaxes(Xl.reshape(10,500,3072), 0,1).reshape(5000,3,32,32)
     Yl = np.array(list([np.identity(10)])*500).reshape(5000,10)
 
     # train model
     model = myModel()
-    raw_input()
-    model.fit(Xl, Yl, batch_size=300, nb_epoch=1000, validation_split=0.2)
+    histl = model.train(Xl,Yl)
+
+    # self training
+    Cu = model.predict_classes(Xu)
+    Yu = np.zeros([len(Cu),nb_classes])
+    for i,c in enumerate(Cu):
+        Yu[i,c] = 1
+    histu = model.train( np.array(list(Xu)+list(Xl)),
+                         np.array(list(Yu)+list(Yl)), )
     model.save(output_model)
 
+    with open('hist_%s' % modelOut, 'w') as f:
+        f.write('Train with Xl\n')
+        f.write(histl.history)
+        f.write('\nTrain with Xu\n')
+        f.write(histu.history)
