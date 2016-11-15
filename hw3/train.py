041f984cd0c1b47e8f7f0af01bcbfbdf74592718
Your Name <you@example.com>
2016-11-15 23:17:53 +0800

    modify train.py  On branch master
     Changes to be committed:
    	modified:   train.py

--- hw3/train.py
+++ hw3/train.py
@@ -6,12 +6,14 @@ import keras.backend
 from keras.models import Sequential
 from keras.layers import *
 from keras.optimizers import SGD, Adam
+from keras.callbacks import EarlyStopping
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 keras.backend.tensorflow_backend.set_session(session)
 
+'''
 class YModel(Sequential):
     def __init__(self, img_channels=3, img_rows=32,
                  img_cols=32, nb_classes=10):
@@ -44,55 +46,47 @@ class YModel(Sequential):
                       metrics=['accuracy'],
                       )
         self.summary()
+'''
+def createModel(self, dim_ordering='th',
+                input_shape=(3,32,32),
+                nb_classes=10, ):
+    
+    model = sequential()
+    model.add(Convolution2D(15, 5,5, dim_ordering=dim_ordering, input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), dim_ordering=dim_ordering))
+    model.add(Dropout(0.65))
 
-class myModel(Sequential):
-    def __init__(self, dim_ordering='th',
-                 input_shape=(3,32,32),
-                 nb_classes=10, 
-                ):
-        super(myModel, self).__init__()
-        self.add(Convolution2D(15, 5,5, dim_ordering=dim_ordering, input_shape=input_shape))
-        self.add(Activation('relu'))
-        self.add(MaxPooling2D(pool_size=(2,2), dim_ordering=dim_ordering))
-        self.add(Dropout(0.65))
+    model.add(Convolution2D(20, 3,3, dim_ordering=dim_ordering))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.65))
+    model.add(Convolution2D(300, 3,3, dim_ordering=dim_ordering))
+    model.add(Activation('relu'))
 
-        self.add(Convolution2D(20, 3,3, dim_ordering=dim_ordering))
-        self.add(Activation('relu'))
-        self.add(Dropout(0.65))
-        self.add(Convolution2D(300, 3,3, dim_ordering=dim_ordering))
-        self.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), dim_ordering=dim_ordering))
+    model.add(Dropout(0.65))
+    '''
+    model.add(Convolution2D(50, 3,3, border_mode='same', dim_ordering='th'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), dim_ordering='th'))
+    '''
+    model.add(Flatten())
+    model.add(Dropout(0.65))
+    model.add(Dense(4*nb_classes))
+    model.add(Activation('relu'))
+    model.add(Dense(2*nb_classes))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_classes))
+    model.add(Activation('relu'))
+    model.add(Activation('softmax'))
 
-        self.add(MaxPooling2D(pool_size=(2,2), dim_ordering=dim_ordering))
-        self.add(Dropout(0.65))
-        '''
-        self.add(Convolution2D(50, 3,3, border_mode='same', dim_ordering='th'))
-        self.add(Activation('relu'))
-        self.add(MaxPooling2D(pool_size=(2,2), dim_ordering='th'))
-        '''
+    adam = Adam(lr=1e-3)
 
-        #self.add(Dropout(0.25))
-
-        
-        self.add(Flatten())
-        self.add(Dropout(0.65))
-        self.add(Dense(4*nb_classes))
-        self.add(Activation('relu'))
-        self.add(Dense(2*nb_classes))
-        self.add(Activation('relu'))
-        self.add(Dense(nb_classes))
-        self.add(Activation('relu'))
-        self.add(Activation('softmax'))
-
-        adam = Adam(lr=1e-3)
-
-        self.compile(loss='categorical_crossentropy', optimizer=adam, init='lecun_uniform', metrics=['accuracy'])
-        self.summary()
+    model.compile(loss='categorical_crossentropy', optimizer=adam, init='lecun_uniform', metrics=['accuracy'])
 
 if __name__ == '__main__':
     path = 'data'
-    output_model = 'trained_model'
-    input_model = output_model
-    test = False
+    modelOut = 'trained_model'
 
     for i,arg in enumerate(sys.argv):
         if arg.startswith('-dir'):
@@ -100,20 +94,28 @@ if __name__ == '__main__':
             if path[-1] == '/':
                 path = path[:-1]
         if arg.startswith('-save'):
-            output_model = sys.argv[i+1]
-        if arg.startswith('-test'):
-            test = True
-        if arg.startswith('-load'):
-            input_model = sys.argv[i+1]
+            modelOut = sys.argv[i+1]
 
     # load/normalize/reordering data
     Xl, Xu, Xt = load_data(path, data=['l'])
     Xl = np.swapaxes(Xl.reshape(10,500,3072), 0,1).reshape(5000,3,32,32)
     Yl = np.array(list([np.identity(10)])*500).reshape(5000,10)
 
-    # train model
-    model = myModel()
-    raw_input()
-    model.fit(Xl, Yl, batch_size=300, nb_epoch=1000, validation_split=0.2)
-    model.save(output_model)
+    # create/train model with labeled data
+    print 'Creating model with labeled data...'
+    model = createModel()
+    model.summary()
+    raw_input('...')
+    earlyStop = EarlyStopping(monitor='val_loss', patience=50)
+    histl = model.fit( Xl, Yl, 
+            batch_size=300, 
+            nb_epoch=1000, 
+            validation_split=0.2, 
+            callback=[earlyStop], )
+
+    # save the model
+    with open('hist_%s' % modelOut, 'w') as f:
+        f.write(histl)
+    raw_input('Save the model '+ modelOut +'?')
+    model.save(modelOut)
 
