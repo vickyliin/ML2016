b4a72abe68304315c16d39dab136d364c4d5e9a5
Your Name <you@example.com>
2016-11-26 22:56:47 +0800

    first submission with score 0.42067 WITHOUT cluster.sh  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   QAQ.csv
    	modified:   data.py
    	new file:   index/MAIN_WRITELOCK
    	new file:   index/MAIN_vnv5nz5mcumffd1y.seg
    	new file:   index/_MAIN_1.toc
    	new file:   table/0.cpk
    	new file:   table/1.cpk
    	new file:   table/2.cpk
    	new file:   table/3.cpk
    	new file:   table/4.cpk
    	new file:   table/5.cpk
    	new file:   table/6.cpk
    	new file:   table/7.cpk
    	new file:   table/8.cpk
    	new file:   table/9.cpk
    	modified:   train.py

--- hw4/data.py
+++ hw4/data.py
@@ -1,15 +1,58 @@
 from nltk.tokenize import word_tokenize
+import pandas as pd
+import _pickle as cpk
+import numpy as np
+from time import time
 
 def loadT(path='data'):
+    # load titles
     if path[-1] != '/':
         path += '/'
     with open(path+'title_StackOverflow.txt', 'r') as f:
-        titles = [ word_tokenize(x) for x in f ]
-    return titles
+        T={}
+        T['title'] = [ x for x in f ]
+        T['term'] = [ word_tokenize(x) for x in T['title'] ]
+        T['tag'] = [0]*len(T['term'])
+        T = pd.DataFrame(T)
+    return T
 
 def loadD(path='data'):
+    # load docs
     if path[-1] != '/':
         path += '/'
     with open(path+'docs.txt', 'r') as f:
-        sentences = [ word_tokenize(x) for x in f if x.strip != '']
+        sentences = [ word_tokenize(x) for x in f if x.strip() != '']
     return sentences
+
+def loadC(path='data'):
+    if path[-1] != '/':
+        path += '/'
+    df = pd.read_csv(path+'check_index.csv', index_col=0)
+    df['ans'] = -1
+    return df
+
+def savetabel(table, path='table/'):
+    size = len(table)/10
+    for i in range(10):
+        with open('%s%d.cpk'%(path,i), 'wb') as f:
+            print('%d,\t%d : %d' % (i,size*i, size*(i+1)))
+            cpk.dump(table[size*i:size*(i+1)], f)
+
+def loadtable(path='table/'):
+    table = []
+    for i in range(10):
+        with open('%s%d.cpk'%(path,i), 'rb') as f:
+            print(i, end=', ', flush=True)
+            table.append(cpk.load(f))
+    table = np.array(table, dtype='bool').reshape(len(table[0])*10, -1)
+    return table
+    
+def save(ans, filename='QAQ.csv'):
+    start = time()
+    with open(filename, 'w') as f:
+        f.write('ID,Ans\n')
+        for i in range(len(ans)):
+            print('%d,%d'%(i,ans[i]), end='\t', flush=True)
+            f.write('%d,%d\n'%(i,ans[i]))
+    ela = time()-start
+    print('\nWritten to file %s!\nTime: %.2f secs'%(filename, ela))
--- hw4/train.py
+++ hw4/train.py
@@ -1,9 +1,83 @@
 from gensim.models import Word2Vec
+from whoosh import index
+from whoosh.fields import *
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.qparser import QueryParser, OrGroup
+from whoosh.searching import Searcher, Results
+import pandas as pd
+import numpy as np
+from time import time
 
-def train(docs, output=None, **kwargs):
+class result():
+    def __init__(self, r=0, searcher=0):
+        if r==0:
+            self.__dict__={ 
+                'title':[],
+                'num':[],
+                'docnum':[],
+                'score':[],
+                'keyterm':[]}
+        else:
+            self.set(r, searcher)
+    def set(self, r, searcher):
+        if type(r) == Results:
+            self.title  = [ x['title'] for x in r ]
+            self.num    = [ x['num'] for x in r ]
+            self.score  = [ x[0] for x in r.top_n ]
+            self.docnum = [ x[1] for x in r.top_n ]
+            key_score = [ searcher.key_terms([x], 'title') \
+                    for x in self.docnum ]
+            self.keyterm = [ list(zip(*x))[0] for x in key_score ]
+    def df(self):
+        return pd.DataFrame(self.__dict__)
+
+def train_w2v(docs, output=None, **kwargs):
     model = Word2Vec(docs, **kwargs)
     if output != None:
         if output==1:
             output='vecmodel.cpk'
         model.save(output)
     return model
+
+def indexGen(T, path='index'):
+    schema = Schema( title=TEXT(stored=True, analyzer=StemmingAnalyzer()),
+                     num=ID(stored=True) )
+    ix = index.create_in(path, schema)
+    with ix.writer() as w:
+        for i in T.index:
+            w.add_document(title=T['title'][i], num=str(i))
+    return 0
+
+def search(query, ix='index', limit=1000):
+    ix = index.open_dir(ix)
+    with ix.searcher() as s:
+        parser = QueryParser('title', schema=ix.schema, group=OrGroup)
+        q = parser.parse(query)
+        rslt = result(s.search(q, limit=limit), s)
+    return rslt
+
+def build_table(T):
+    ans = np.identity(20000, dtype='bool')
+    for i in T.index:
+        print(i, end=', ', flush=True)
+        start=time()
+        ans[i][ search(T['title'][i]).docnum ] = 1
+        ela = time()-start
+        print('%.2f sec' % ela, end='\t', flush=True)
+    return ans
+
+def check(C, table):
+    answer = np.zeros(len(C), dtype='bool')
+    start = time()
+    for i in C.index:
+        print(i, end=',', flush=True)
+        answer[i] = table[ C['x_ID'][i] , C['y_ID'][i] ]
+        print('%d'%answer[i], end='\t', flush=True)
+    ela = time()-start
+    print('\nTime: %.2f secs'%els)
+    return answer
+
+def make_sentVec(docs, wordmodel):
+    return sentVecs
+def train_sentModel(docVecs, output=None, **kwargs):
+    return 0
